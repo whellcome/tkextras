abb2310d4fc66b8269c3a6b3d7ab935b33faf46b
st.dietmar <st.dietmar1@t-online.de>
2025-01-22 23:32:31 +0100

    v1.1.x: Sphinx documentation

--- tkextras/widgets_render.py
+++ tkextras/widgets_render.py
@@ -1,5 +1,6 @@
 """
 Contains the implementation of the class WidgetsRender
+
 """
 import tkinter as tk
 
@@ -8,13 +9,15 @@ class WidgetsRender:
     """
     Perform element creation and rendering in one command. Without creating a variable unnecessarily.
     Combines general parameters for the arrangement of elements and parameters for a specific element.
+
     """
     def __init__(self, render_params: dict | None = None, *args, **options):
         """
         Initialization of the Frame, description of the main elements
+
         :param render_params: General parameters for the arrangement of elements can be set externally
-        :param args:
-        :param options:
+        :param args: standard arguments of the tkinter widget
+        :param options: standard options of the tkinter widget
         """
         super().__init__(*args, **options)
         if render_params is None:
@@ -25,6 +28,7 @@ class WidgetsRender:
         """
         Combines the packing parameters pack_params of the object with the parameters of the main object
         of class WidgetsRender. Brings it in line with the rendering function.
+
         :param pack_params: parameters pack_params of the current object.
         :param func: the rendering function.
         :return: combined packing parameters.
@@ -43,6 +47,7 @@ class WidgetsRender:
     def rgrid(self, obj: tk.Widget, render_params=None):
         """
         Implementation of the tk.Widget.grid() method as a wrapper function
+
         :param obj: Element to rendering
         :param render_params: Dictionary with element parameters
         :return: Rendered element
@@ -54,6 +59,7 @@ class WidgetsRender:
     def rpack(self, obj: tk.Widget, render_params=None):
         """
         Implementation of the tk.Widget.pack() method as a wrapper function
+
         :param obj: Element to rendering
         :param render_params: Dictionary with element parameters
         :return: Rendered element
@@ -65,6 +71,7 @@ class WidgetsRender:
     def rplace(self, obj: tk.Widget, render_params=None):
         """
         Implementation of the tk.Widget.place() method as a wrapper function.
+
         :param obj: Element to rendering
         :param render_params: Dictionary with element parameters
         :return: Rendered element
